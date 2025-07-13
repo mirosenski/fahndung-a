2a1ba9f4fccd83680c392b6ee27676fd878116ba
Igor Mirosenski <igor.mirosenski@polizei-bw.de>
2025-07-13 12:25:18 +0200

    🔐 Update Auth-Konfiguration

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,8 +1,7 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
-import { db } from "~/server/db";
+// import { db } from "~/server/db";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -46,7 +45,7 @@ export const authConfig = {
      * @see https://next-auth.js.org/providers/github
      */
   ],
-  adapter: PrismaAdapter(db),
+  // adapter: PrismaAdapter(db),
   callbacks: {
     session: ({ session, user }) => ({
       ...session,
