8c32e3f49fdfc619a67804c9570de744c84dcef3
Igor Mirosenski <igor.mirosenski@polizei-bw.de>
2025-07-13 12:23:29 +0200

    📝 Update Post-API-Router

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -15,26 +15,7 @@ export const postRouter = createTRPCRouter({
       };
     }),
 
-  create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      return ctx.db.post.create({
-        data: {
-          name: input.name,
-          createdBy: { connect: { id: ctx.session.user.id } },
-        },
-      });
-    }),
-
-  getLatest: protectedProcedure.query(async ({ ctx }) => {
-    const post = await ctx.db.post.findFirst({
-      orderBy: { createdAt: "desc" },
-      where: { createdBy: { id: ctx.session.user.id } },
-    });
-
-    return post ?? null;
-  }),
-
+  // Alle Post-Routen, die auf ctx.db.post zugreifen, sind deaktiviert, da das Model im Prisma-Schema fehlt.
   getSecretMessage: protectedProcedure.query(() => {
     return "you can now see this secret message!";
   }),
