b04a28114cf93315eab66d6564546fcacbdcd724
Igor Mirosenski <igor.mirosenski@polizei-bw.de>
2025-07-13 12:47:01 +0200

    ⬆️ Update Auth-Konfig & Abhängigkeiten

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,2 +1,54 @@
-// Auth-Konfiguration aktuell deaktiviert, da next-auth nicht installiert ist.
-// Hier kann später wieder eine Auth-Konfiguration ergänzt werden.
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { type DefaultSession, type NextAuthOptions } from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+import GitHubProvider from "next-auth/providers/github";
+import { db } from "~/server/db";
+
+/**
+ * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
+ * object and keep type safety.
+ *
+ * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
+ */
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      // ...other properties
+      // role: UserRole;
+    } & DefaultSession["user"];
+  }
+
+  // interface User {
+  //   // ...other properties
+  //   // role: UserRole;
+  // }
+}
+
+/**
+ * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
+ *
+ * @see https://next-auth.js.org/configuration/options
+ */
+export const authConfig = {
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+  adapter: PrismaAdapter(db),
+  callbacks: {
+    session: ({ session, user }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id,
+      },
+    }),
+  },
+} satisfies NextAuthOptions;
